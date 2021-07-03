8e718cb21072682039a7c72ae5395ef2b3be9ed5
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2021-07-03 02:12:03 -0300

    leason 19

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#include <cmath>
 #include "LTexture.h"
 #include "LButton.h"
 #include "../Debugging/Debug.h"
@@ -10,6 +11,9 @@
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+//Analog joystick dead zone
+const int JOYSTICK_DEAD_ZONE = 8000;
 
 //Key press surfaces constants
 enum KeyPressSurfaces
@@ -51,10 +55,13 @@ LTexture gLeftTexture;
 LTexture gRightTexture;
 LTexture gPressTexture;
 
+LTexture gArrowTexture;
 
 //Buttons objects
 LButton gButtons[ TOTAL_BUTTONS ];
 
+//Game Controller 1 handler
+SDL_Joystick* gGameController = nullptr;
 
 //The image we will load and show on the screen
 int main(int argc, char* args[] )
@@ -84,6 +91,10 @@ int main(int argc, char* args[] )
             //Current rendererd texture
             LTexture* currentTexture = nullptr;
 
+            //Normalized direction
+            int xDir = 0;
+            int yDir = 0;
+
             //While application is running
             while(!quit)
             {
@@ -104,6 +115,49 @@ int main(int argc, char* args[] )
                             case SDLK_ESCAPE:
                                 quit = true;
                                 break;
+                        }
+                    }
+                    else if (e.type == SDL_JOYAXISMOTION)
+                    {
+                        //Motion on controller 0
+                        if (e.jaxis.which == 0)
+                        {
+                            //X axis motion
+                            if (e.jaxis.axis == 0)
+                            {
+                                //Left of dead zone
+                                if( e.jaxis.value < -JOYSTICK_DEAD_ZONE )
+                                {
+                                    xDir = -1;
+                                }
+                                //Right of dead zone
+                                else if ( e.jaxis.value > JOYSTICK_DEAD_ZONE )
+                                {
+                                    xDir = 1;
+                                }
+                                else
+                                {
+                                    xDir = 0;
+                                }
+                            }
+                            //Y axis motion
+                            else if (e.jaxis.axis == 1)
+                            {
+                                //Below of dead zone
+                                if( e.jaxis.value < -JOYSTICK_DEAD_ZONE )
+                                {
+                                    yDir = -1;
+                                }
+                                //Above of dead zone
+                                else if ( e.jaxis.value > JOYSTICK_DEAD_ZONE )
+                                {
+                                    yDir = 1;
+                                }
+                                else
+                                {
+                                    yDir = 0;
+                                }
+                            }
                         }
                     }
 
@@ -140,14 +194,18 @@ int main(int argc, char* args[] )
                 SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
                 SDL_RenderClear( gRenderer );
 
-                // //Render buttons
-                // for (auto & gButton : gButtons)
-                // {
-                //     gButton.render();
-                // }
+                //Calculate angle
+                double joystickAngle = atan2((double)yDir, (double)xDir) * (180.0 / M_PI);
 
-                //Render  current textur
-                currentTexture->render(0, 0);
+                //Correct angle
+                if (xDir == 0 && yDir == 0)
+                {
+                    joystickAngle = 0;
+                }
+
+                //Render joystick 8 way angle
+                gArrowTexture.render((SCREEN_WIDTH - gArrowTexture.getWidth()) / 2, (SCREEN_HEIGHT - gArrowTexture.getHeight()) / 2,
+                                     nullptr, joystickAngle);
 
                 //Update screen
                 SDL_RenderPresent( gRenderer );
@@ -168,7 +226,7 @@ bool init()
     bool success = true;
 
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK ) < 0 )
     {
 //        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
         Debug::Log("SDL could not initialize! SDL_Error: %s", SDL_GetError() );
@@ -181,6 +239,21 @@ bool init()
         {
 //            printf( "Warning: Linear texture filtering not enabled!" );
             Debug::Log( "Warning: Linear texture filtering not enabled!" );
+        }
+
+        //Check for joysticks
+        if (SDL_NumJoysticks() < 1)
+        {
+            Debug::Log("Warning: No joysticks connected!");
+        }
+        else
+        {
+            //Load joystick
+            gGameController = SDL_JoystickOpen( 0 );
+            if ( gGameController == nullptr )
+            {
+                Debug::Log("Warning: Unable to open game controller! SDL Error: %s");
+            }
         }
 
         //Create window
@@ -263,6 +336,12 @@ bool loadMedia()
         success = false;    
     }
 
+    if (!gArrowTexture.loadFromFile("../assets/arrow.png"))
+    {
+        Debug::Log("Falied to load arrow sprite! SDL_IMG Error: %s!", IMG_GetError());
+        success = false;
+    }
+
     // if (!gButtonSpriteSheetTexture.loadFromFile("../assets/button.png"))
     // {
     //     Debug::Log("Failed to load sprite! SDL_ttf Error: %s!", TTF_GetError());
@@ -291,9 +370,13 @@ void close()
     //Free loaded images
     gButtonSpriteSheetTexture.free();
 
-    //Free global font
-    TTF_CloseFont( gFont );
-    gFont = nullptr;
+//    //Free global font
+//    TTF_CloseFont( gFont );
+//    gFont = nullptr;
+
+    //Close game controller
+    SDL_JoystickClose( gGameController );
+    gGameController = nullptr;
 
     //Destroy window
     SDL_DestroyRenderer( gRenderer );
